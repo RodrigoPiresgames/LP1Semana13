47a1ffde10e4ef3c0eb4b0d22549d9075a6c1042
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2022-06-02 16:32:28 +0100

    Verification add

---
+++ Palidrome/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Palidrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                Console.WriteLine($"{arg}");
+                IsPalidrome(arg);
+            }
+        }
+
+        private bool IsPalidrome(string word)
+        {
+            if (word == null)
+            {
+                word = word ?? throw new ArgumentNullException("Test");
+            }
+
+            return false;
+        }
+
+    }
+}
